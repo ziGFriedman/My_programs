5c732cd7922ba3f86150a008d3f49029824381d7
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-05-03 10:12:18 +0500

    Update Equation_of_line.py

--- Equation_of_line.py
+++ Equation_of_line.py
@@ -12,7 +12,7 @@
 print("Координаты точки A(x1; y1):")
 x1 = float(input("\tx1 = "))
 y1 = float(input("\ty1 = "))
- 
+
 print("Координаты точки B(x2; y2):")
 x2 = float(input("\tx2 = "))
 y2 = float(input("\ty2 = "))
