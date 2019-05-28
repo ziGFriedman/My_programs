6295809903df7592eb3f5146ac4f73a13473f8fa
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-05-28 10:47:41 +0500

    Update with_forms.py

--- Flask/with_forms.py
+++ Flask/with_forms.py
@@ -6,7 +6,9 @@ from wtforms import StringField
 from wtforms import validators
 
 class ContactForm(FlaskForm):
-    name = StringField(label='Name', validators=[validators.Length(min=4, max=25)])    # ожидает в форме значения по названию переменных
+    name = StringField(label='Name', validators=[
+        validators.Length(min=4, max=25)
+        ])    # ожидает в форме значения по названию переменных
     email = StringField(label='E-mail', validators=[
         validators.Length(min=6, max=35),
         validators.Email()    # правильность записи емайла
@@ -18,9 +20,11 @@ class ContactForm(FlaskForm):
         ])
 
 app = Flask(__name__)
-app.config.update(DEBUG=True, SECRET_KEY='This key must be secret!', WTF_CSRF_ENABLED=False)
+app.config.update(DEBUG=True, SECRET_KEY='This key must be secret!',
+    WTF_CSRF_ENABLED=False)
 
-@app.route('/', methods=['GET', 'POST'])    # форма обязательно наследуется от класса с параметром FlaskFrom
+# форма обязательно наследуется от класса с параметром FlaskFrom
+@app.route('/', methods=['GET', 'POST'])    
 def home():
     if request.method == 'POST':
         print(request.form)    # print - передает значения в консоль
