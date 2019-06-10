b57c1fc21b90166617f315381eff1ddcfdd85781
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-06-10 16:44:54 +0500

    Update outside_request2.py

--- Flask/outside_request2.py
+++ Flask/outside_request2.py
@@ -6,9 +6,14 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    print(request)
     return ('Hello World!', 200)
 
-print(request, type(request), request.method)
+# print(request, type(request), request.method) - случится ошибка т.к. реквест не
+# определен в функции (он имеет контекст)
+
+# with app.test_request_context('/?next=http://example.com/'):
+#     print(request, type(request), request.method)
 
 if __name__ == '__main__':
     app.run()
