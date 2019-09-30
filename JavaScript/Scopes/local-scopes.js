0e87e7a0751b87f095237fec07da2e08d28039ab
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-09-30 16:17:45 +0500

    Create local-scopes.js

---
+++ JavaScript/Scopes/local-scopes.js
@@ -0,0 +1,10 @@
+(function (document, window) {
+  function scoped() {
+    console.log('scoped');
+    console.log(window, document);
+    console.log(String(scoped));
+  }
+
+  scoped();
+  // globalFunction();
+})
